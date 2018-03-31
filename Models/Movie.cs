6ff106eaffdd73d78593a3fdcb397faf511ea5cb
MooPooNGit <moopoon.noi@gmail.com>
2018-03-31 19:27:06 +0700

    Initialize Repository

---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public Movie()
+        {
+        }
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
